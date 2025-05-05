f8e37f63ee2f474501a5fbde92c08b4a0b519877
Hoang Dung <dhdung71@gmail.com>
2025-05-05 18:28:05 +0700

    climbing_stair to stepping_stair

--- backend/src/models/activityModel.js
+++ backend/src/models/activityModel.js
@@ -6,7 +6,7 @@ const ACTIVITY_COLLECTION_NAME = 'activities'
 const ACTIVITY_SCHEMA = Joi.object({
   userId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   timestamp: Joi.date().required(),
-  activity: Joi.string().valid('running', 'walking', 'stair_climbing', 'idle').required(),
+  activity: Joi.string().valid('running', 'walking', 'stepping_stair', 'idle').required(),
   steps: Joi.number().integer().min(0).required(),
   caloriesBurned: Joi.number().optional().allow(null),
   createdAt: Joi.date().default(() => new Date())
